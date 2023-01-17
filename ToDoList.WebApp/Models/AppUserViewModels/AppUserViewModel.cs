92c7f990baaf7494390131c1ff4de64d86b43e80
Tomasz789 <lachowiec_95@wp.pl>
2023-01-17 16:27:11 +0100

    Changes in vm

--- ToDoList.WebApp/Models/AppUserViewModels/AppUserViewModel.cs
+++ ToDoList.WebApp/Models/AppUserViewModels/AppUserViewModel.cs
@@ -6,17 +6,13 @@ namespace ToDoList.WebApp.Models.AppUserViewModels
 {
     public class AppUserViewModel
     {
-        [Required]
         [Display(Name = "First name")]
         public string FirstName { get; set; }
-        [Required]
         [Display(Name = "Last name")]
         public string LastName { get; set; }
 
-        [Required]
         public string City { get; set; }
 
-        [Required]
         public string Country { get; set; }
 
         [Display(Name = "Birth date")]
