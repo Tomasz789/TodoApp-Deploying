3781b8828cbef1da7aeb71ab64b82aad083b280d
Tomasz789 <lachowiec_95@wp.pl>
2023-01-19 12:27:19 +0100

    Test for smtp client.

---
+++ TodoApp.Testing/Email/EmailSenderTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TodoApp.EmailService.Services;
+
+namespace TodoApp.Testing.Email
+{
+    [TestFixture]
+    public class EmailSenderTest
+    {
+        private IEmailSender sender;
+
+        [SetUp]
+        public void Init()
+        {
+            sender = new EmailSender();
+        }
+
+        [Test]
+        public void SendEmailPassed_Test()
+        {
+            var res = sender.SendEmail("testmail@test.com", "Error1", "Unable to get task lists");
+
+            Assert.AreEqual(1, res);
+        }
+    }
+}
