6dc2433c247152b6356d58243aad8115618d836c
Tomasz789 <lachowiec_95@wp.pl>
2023-01-19 12:30:29 +0100

    Message for email implementation

---
+++ Todo.Domain.Entities/Message.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using Todo.Domain.Entities.ReportTypes;
+
+namespace Todo.Domain.Entities
+{
+    public class Message
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public ReportType ReportType { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public virtual AppUser User { get; set; }
+    }
+}
