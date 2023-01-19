cd0f8cc53e8b757d19e24ea3a1d35946f99a2445
Tomasz789 <lachowiec_95@wp.pl>
2023-01-19 12:29:32 +0100

    Report types for Message

---
+++ Todo.Domain.Entities/ReportTypes/ReportType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Todo.Domain.Entities.ReportTypes
+{
+    public enum ReportType
+    {
+        ERROR, INFO, OTHER
+    }
+}
