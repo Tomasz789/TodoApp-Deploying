3a672c6e4a662ff85945905d951d804c47ad68b0
Tomasz789 <lachowiec_95@wp.pl>
2023-05-08 16:45:05 +0200

    Captcha entities - settings and response

---
+++ TodoApp.WeatherApiHelper/Models/GoogleCaptcha/GoogleCaptchaResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoApp.WeatherApiHelper.Models.GoogleCaptcha
+{
+    public class GoogleCaptchaResponse
+    {
+        public bool Success { get; set; }
+
+        public DateTime Challenge_Ts { get; set; }
+
+        public string HostName { get; set; }
+
+        public string [] ErrorCodes { get; set; }
+    }
+}
---
+++ TodoApp.WeatherApiHelper/Models/GoogleCaptcha/GoogleCaptchaSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoApp.WeatherApiHelper.Models.GoogleCaptcha
+{
+    public class GoogleCaptchaSettings
+    {
+        public string ClientKey { get; set; }
+
+        public string SecretKey { get; set; }
+    }
+}
