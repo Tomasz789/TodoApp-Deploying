6102f39cfb94519338fc377216140c5b3747fbd5
Tomasz789 <lachowiec_95@wp.pl>
2023-05-08 16:43:06 +0200

    Configs for recaptcha api

---
+++ TodoApp.WeatherApiHelper/Configs/CaptchaConfig.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace TodoApp.WeatherApiHelper.Configs
+{
+    public class CaptchaConfig : IConfig
+    {
+        private readonly IConfiguration _config;
+
+        public CaptchaConfig()
+        {
+            var builder = new ConfigurationBuilder().AddJsonFile(@"D:\Projekt\TodoApp-FinalVersion\TodoApp-Deploying\TodoApp.WeatherApiHelper\captchaconfig.json");
+            _config = builder.Build();
+        }
+
+        public string SiteKey => _config["GoogleCaptcha:ClientKey"];
+
+        public string SecretKey => _config["GoogleCaptcha:SecretKey"];
+        public IConfiguration Config
+        {
+            get { return _config; }
+        }
+    }
+}
---
+++ TodoApp.WeatherApiHelper/Configs/ICaptchaConfig.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Text;
+using TodoApp.WeatherApiHelper.Models.GoogleCaptcha;
+
+namespace TodoApp.WeatherApiHelper.Configs
+{
+    public interface IConfig
+    {
+        public string SiteKey { get; }
+
+        public string SecretKey { get; }
+        public IConfiguration Config { get; }
+    }
+}
